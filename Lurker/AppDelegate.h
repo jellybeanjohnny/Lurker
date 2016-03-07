574ff151f1a709daac324e56f3a2701a264d35ae
Matt Amerige <matt@wubbyland.com>
2016-03-06 19:56:30 -0500

    Initial Commit

---
+++ Lurker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lurker
+//
+//  Created by Matt Amerige on 3/6/16.
+//  Copyright © 2016 Wubbyland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lurker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lurker
+//
+//  Created by Matt Amerige on 3/6/16.
+//  Copyright © 2016 Wubbyland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
